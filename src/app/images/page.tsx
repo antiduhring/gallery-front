41ae758f462fa18a712237154bf611e1d7b937ee
antiduhring <83927931+antiduhring@users.noreply.github.com>
2023-12-19 00:38:43 +0100

    Fetch data from server Local env

--- src/app/images/page.tsx
+++ src/app/images/page.tsx
@@ -1,6 +1,15 @@
 import {getServerUrl} from "@/app/_lib/utils";
 
-export default function Images() {
-    const serverUrl = getServerUrl()
-    return (<><h1>SERVER URL: {serverUrl}</h1></>)
+async function getItems() {
+    const res = await fetch(getServerUrl(), {cache: 'no-cache'})
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data')
+    }
+    return res.json()
+}
+
+export default async function Images() {
+    const items = await getItems()
+    return (<><h1>SERVER URL: {getServerUrl()} ITEMS: {JSON.stringify(items)} </h1></>)
 }
